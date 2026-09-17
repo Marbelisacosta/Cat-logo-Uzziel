d3d2f8215ae80d27de81c245dfac6824f71ed892
marbelis acosta <acostamarbelis2005@gmail.com>
2026-09-17 13:00:25 +0000

    COLOCA LAS IMAGENES DE LOS PRODUCTOS QUE TENIA LOS QUE SUBI MEDIANTE UN

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -308,7 +308,7 @@ export const products: Product[] = [
     price: 0,
     category: 'Gorras',
     stock: 100,
-    description: 'Gorras personalizables en varios colores. ¡Diseño GRATIS!',
+    description: 'Gorras personalizables en varios colores. *Consultar Precio por Medida.* ¡Diseño GRATIS!',
     imagePlaceholderId: 'gorra-1',
   },
 
@@ -414,7 +414,7 @@ export const products: Product[] = [
     price: 0,
     category: 'Microperforado',
     stock: 999,
-    description: 'Vinilo microperforado ideal para vidrios de locales o vehículos. Permite ver hacia afuera mientras muestra tu publicidad hacia adentro.\\n\\n*El precio varía según las medidas y el diseño solicitado.* ¡Diseño GRATIS!',
+    description: 'Vinilo microperforado ideal para vidrios de locales o vehículos. Permite ver hacia afuera mientras muestra tu publicidad hacia adentro.\n\n*Consultar Precio por Medida.* ¡Diseño GRATIS!',
     imagePlaceholderId: 'microperforado-img',
   },
 
